54acb14bc7d8bca90906f4788816da038ecc9ffe
Benjamin Feng <contact@fengb.me>
2019-02-25 11:34:47 -0600

    Import react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import PokemonPreview from "./views/PokemonPreview";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
 
 export function App() {
   return (
     <main>
       <h1>Pokemon!</h1>
-      <PokemonPreview />
+      <Router>
+        <Route path="/" component={PokemonPreview} />
+      </Router>
     </main>
   );
 }
