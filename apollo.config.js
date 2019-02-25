e7a2d9075f99cc5e74efe2393426a128b135dc80
Benjamin Feng <contact@fengb.me>
2019-02-25 09:03:53 -0600

    Apollo pushups

---
+++ apollo.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  client: {
+    service: {
+      name: "pokemon",
+      url: "https://graphql-pokemon.now.sh/",
+      /*
+      // optional headers
+      headers: {
+        authorization: "Bearer lkjfalkfjadkfjeopknavadf"
+      },
+      // optional disable SSL validation check
+      skipSSLValidation: true
+      */
+    }
+  }
+};
---
+++ src/queries/pokemons.ts
@@ -0,0 +1,10 @@
+import { gql } from "apollo-boost";
+export default gql`
+  query Pokemons($first: Int!) {
+    pokemons(first: $first) {
+      id
+      number
+      name
+    }
+  }
+`;
