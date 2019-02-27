22bd03e9f5b315e85b80134c9cd2cc9d3689e673
Benjamin Feng <contact@fengb.me>
2019-02-26 18:52:44 -0600

    Updates

---
+++ precache-manifest.273ecc6b6c288dfb12d3a132843f8a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eeeb108b2980d3dbd73",
+    "url": "/pokemon-graphql/static/css/main.c25378ce.chunk.css"
+  },
+  {
+    "revision": "8eeeb108b2980d3dbd73",
+    "url": "/pokemon-graphql/static/js/main.8eeeb108.chunk.js"
+  },
+  {
+    "revision": "38d452858ebdbf61df70",
+    "url": "/pokemon-graphql/static/js/runtime~main.38d45285.js"
+  },
+  {
+    "revision": "c21a8905248d06d80407",
+    "url": "/pokemon-graphql/static/js/2.c21a8905.chunk.js"
+  },
+  {
+    "revision": "9825a1fdeef8da412c4e970faa224e3d",
+    "url": "/pokemon-graphql/index.html"
+  }
+];
